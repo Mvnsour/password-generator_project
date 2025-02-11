622cd69f9a74ad4949dd356485b521bc3bc33984
Mansour Mbengue <mbgmansour13@gmail.com>
2025-02-11 03:13:38 +0100

    feat: first function created to ask how many character the user wants

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import { prompt } from './prompt.js';
+
+function askCharsLength() {
+  let charLength = Number(prompt('How many characters do you want ? (8-36)\n'));
+  
+  if (Number.isNaN(charLength) || charLength > 36 || charLength < 8) {
+  throw new Error("Please you must write a number between 8 and 36. Try again.\n");
+  }
+  return charLength;
+}
