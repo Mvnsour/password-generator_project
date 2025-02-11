7385ef800f81204e2ef9fcb5a14d26c06b88b1fd
Mansour Mbengue <mbgmansour13@gmail.com>
2025-02-11 11:50:04 +0100

    feat: function created to ask if the user wants special characters in his password

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ function askCharsLength() {
   if (Number.isNaN(charLength) || charLength > 36 || charLength < 8) {
   throw new Error("Please you must write a number between 8 and 36. Try again.\n");
   }
+
   return charLength;
+}
+
+function askSpecialChars() {
+  let specialChars = prompt('Do you want special characters ? (y/n)\n');
+  
+  if (specialChars !== 'y' && specialChars !== 'n') {
+    throw new Error(`Please you must write "y" for "yes" or "n" for "no". Try again.\n`);
+  }
+
+  return specialChars;
 }
